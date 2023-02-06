7dc0667477492cb4e6e235e10c7e75b67ad7b562
Maaloum <50062179+maaloum@users.noreply.github.com>
2023-02-06 18:54:28 +0300

    fix linters

--- Student.rb
+++ Student.rb
@@ -1,9 +1,10 @@
-require './Person'
+require 'Person'
 class Student < Person
   def initialize(classroom, name, age, parent_permission)
     super(name, age, parent_permission)
     @classroom = classroom
   end
+
   def play_hooky
     "¯\(ツ)/¯"
   end
--- Teacher.rb
+++ Teacher.rb
@@ -1,9 +1,10 @@
-require './Person'
+require 'Person'
 class Teacher < Person
   def initialize(specialization, name, age, parent_permission)
     super(name, age, parent_permission)
     @specialization = specialization
   end
+
   def can_use_services?
     true
   end
