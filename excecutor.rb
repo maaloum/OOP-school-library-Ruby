62be32bed33332e07cd495f1be3d83a253441d10
Maaloum <50062179+maaloum@users.noreply.github.com>
2023-02-08 14:13:33 +0300

    fixing linters

--- excecutor.rb
+++ excecutor.rb
@@ -1,10 +1,10 @@
 require_relative 'person'
-require_relative './capitalizeDecorator '
+require_relative './capitalizeDecorator'
 require_relative 'trimmerDecorator'
 
 person = Person.new(22, 'maximilianus')
 p person.correct_name
 capitalized_person = CapitalizeDecorator.new(person)
 p capitalized_person.correct_name
-capitalized_trimmedPerson = TrimmerDecorator.new(capitalized_person)
-p capitalized_trimmedPerson.correct_name
+capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+p capitalized_trimmed_person.correct_name
