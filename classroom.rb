0b433fb3882356c24a7139f6487a5f3c4a49e5a7
Maaloum <50062179+maaloum@users.noreply.github.com>
2023-02-08 17:56:02 +0300

    create classroom

---
+++ classroom.rb
@@ -0,0 +1,9 @@
+require_relative 'my_student'
+class Classroom
+  def initialize(label)
+    @label = label
+  end
+  attr_accessor :@label
+  has_many :student
+
+end
--- my_student.rb
+++ my_student.rb
@@ -1,11 +1,11 @@
 require_relative 'my_person'
-
+require_relative 'classroom'
 class Student < Person
   def initialize(classroom, name, age, parent_permission)
     super(name, age, parent_permission)
     @classroom = classroom
   end
-
+  belongs-to :classroom
   def play_hooky
     '¯\(ツ)/¯'
   end
