ac5c730cfcceeb70c3e49507798a5caa60823963
Maaloum <50062179+maaloum@users.noreply.github.com>
2023-02-10 16:43:07 +0300

    add basic UI

--- excecutor.rb
+++ excecutor.rb
@@ -1,10 +1,11 @@
-require_relative 'person'
-require_relative './capitalizeDecorator'
-require_relative 'trimmerDecorator'
+require_relative 'app'
 
-person = Person.new(22, 'maximilianus')
-person.correct_name
-capitalized_person = CapitalizeDecorator.new(person)
-capitalized_person.correct_name
-capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
-capitalized_trimmed_person.correct_name
+def main
+  app = App.new
+  loop do
+    app.run
+    puts
+  end
+end
+
+main
